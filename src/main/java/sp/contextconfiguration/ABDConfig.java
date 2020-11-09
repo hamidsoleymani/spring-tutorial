91357d663b183e9659282a4d106b29d4f633aac3
hsol <hamid.soleymani@gmx.de>
2020-11-09 09:50:34 +0100

    spring doc

--- src/main/java/sp/contextconfiguration/ABDConfig.java
+++ src/main/java/sp/contextconfiguration/ABDConfig.java
@@ -6,16 +6,23 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 import javax.annotation.PostConstruct;
+/*
 
+
+@ContextConfiguration is only usable in tests in other code
+it doesn't do anything. @Import is useless on a test and will only be useful
+on @Configuration classes to import other configuration classes
+(with @ImportResource you can import XML configuration files).
+ */
 @Configuration
 @Import({BeanA.class, BeanB.class, BeanD.class})
-public class AppConfig {
+public class ABDConfig {
 
     @Autowired
     private ConfigurableEnvironment environment;
 
     @PostConstruct
-    public void construct(){
+    public void construct() {
         environment.addActiveProfile("test");
         environment.addActiveProfile("prod");
         environment.addActiveProfile("qa");
